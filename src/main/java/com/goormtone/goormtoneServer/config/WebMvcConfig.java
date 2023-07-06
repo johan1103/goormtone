9d9b3b294057890387ad97b684fc6817724aade6
johan1103 <johan1103@naver.com>
2023-07-07 04:22:09 +0900

    [Fix] cors

--- src/main/java/com/goormtone/goormtoneServer/config/WebMvcConfig.java
+++ src/main/java/com/goormtone/goormtoneServer/config/WebMvcConfig.java
@@ -19,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
             .allowedHeaders("headers")
             .allowCredentials(true)
