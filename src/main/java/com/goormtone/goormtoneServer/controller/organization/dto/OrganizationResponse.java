6aa31aaa45c76ac58e61fadcd30b2412db1372b0
johan1103 <johan1103@naver.com>
2023-07-15 01:17:47 +0900

    [Feat] edit organization detail api

--- src/main/java/com/goormtone/goormtoneServer/controller/organization/dto/OrganizationResponse.java
+++ src/main/java/com/goormtone/goormtoneServer/controller/organization/dto/OrganizationResponse.java
@@ -2,16 +2,31 @@ package com.goormtone.goormtoneServer.controller.organization.dto;
 
 import com.goormtone.goormtoneServer.domain.nonProfit.NonProfitOrganization;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrganizationResponse {
   private String name;
-  private Integer accumulatePoint;
+  private String description;
+  private LocalDate start;
+  private LocalDate end;
+  private Integer point;
+  private Integer maxPoint;
   public static OrganizationResponse ofOrganization(NonProfitOrganization organization){
-    return new OrganizationResponse(organization.getName(),organization.getPoint());
+    return new OrganizationResponseBuilder()
+            .name(organization.getName())
+            .description(organization.getDescription())
+            .start(organization.getStartDate())
+            .end(organization.getEndDate())
+            .point(organization.getPoint())
+            .maxPoint(organization.getMaxPoint())
+            .build();
   }
 }
